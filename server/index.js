480acd6479ec0f47ef94ceebb08e8c0394a2ed5b
inkersal501 <inkersal501@gmail.com>
2025-02-21 21:32:15 +0530

    complete backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+const taskRoutes = require("./routes/task.route");  
 
 const app = express();
 const PORT = 8082;
-const DB_URI = 'mongodb://localhost:27017/task-manager';
+// const DB_URI = 'mongodb://localhost:27017/task-manager';
+const DB_URI = 'mongodb+srv://inkersal501:test123@cluster0.uoc8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(DB_URI, {
     "useNewUrlParser" :true,
@@ -12,9 +14,17 @@ mongoose.connect(DB_URI, {
 }).then(()=> console.log("DB Connected"))
 .catch((error)=> console.log("Error Connection Database: ", error));
 
-app.use(cors);
+   
+app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => { 
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+app.use("/tasks", taskRoutes);
+
 app.listen(PORT, ()=> {
- console.log(`Server is listening on PORT ${PORT}`);
-})
+    console.log(`Server is listening on PORT ${PORT}`);
+});  
